32f0c066900ab5628c0abfa72cd7d5e9096c1f83
Chely Ho <adept_inept@yahoo.com>
2017-11-09 18:06:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,11 @@ function Scooter(year, color, model){
 }
 
 function Driver(name, age, experience){
-this.name = name;
-this.age = age;
-this.experience = experience;
+  this.name = name;
+  this.age = age;
+  this.experience = experience;
+}
+
+function PickupLocation(){
+  
 }
